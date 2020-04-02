cd64fbede293c2d29d1bd469f02529df6afa6211
cmac99 <colinmacrury99@gmail.com>
2020-04-02 18:20:04 +0100

    Chpt3 Tests Passed

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
 def index(request):
-    return HttpResponse("Rango says hey there partner")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+
 
 # Create your views here.
