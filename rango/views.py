f8f513ad8ed9c1100118ebbdf95a4adea3c76332
cmac99 <colinmacrury99@gmail.com>
2020-05-02 11:53:03 +0100

    Chapt5 Tests Passed

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def about(request):
 
     return render(request, 'rango/about.html')
 
 def index(request):
-    #dict established to allow passing of template engine
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     #returning a rendered response
     return render(request, 'rango/index.html', context=context_dict)
